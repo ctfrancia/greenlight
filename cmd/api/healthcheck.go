b36cdec4c1131c2261e0f8521b29e2887a95cae6
Christian Francia <christian@Christians-MacBook-Pro.local>
2021-05-21 16:13:46 +0200

    alter json response of healthcheck

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -5,13 +5,22 @@ import (
 )
 
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	data := map[string]string{
-		"status":      "available",
-		"environment": app.config.env,
-		"version":     "1.0.0",
+	/*
+		data := map[string]string{
+			"status":      "available",
+			"environment": app.config.env,
+			"version":     "1.0.0",
+		}
+	*/
+	env := envelope{
+		"status": "available",
+		"system_info": map[string]string{
+			"environment": app.config.env,
+			"version":     version,
+		},
 	}
 
-	err := app.writeJSON(w, http.StatusOK, data, nil)
+	err := app.writeJSON(w, http.StatusOK, env, nil)
 	if err != nil {
 		app.logger.Println(err)
 		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
